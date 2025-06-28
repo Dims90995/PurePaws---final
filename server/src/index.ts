b8e260e1b6c2d27da8d22deea9e0494d8f31361a
Guy <Guy9502@Gmail.com>
2025-06-28 15:10:47 +0300

    changing server's structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,6 @@
-import express from 'express';
-import cors from 'cors';
+import app from './app';
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api', (_req, res) => {
-  res.json({ message: 'Server is running!' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
