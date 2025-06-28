4113e92aafa83dea261b48c63778b21e20dd4fc4
Guy <Guy9502@Gmail.com>
2025-06-28 14:48:12 +0300

    Define server side and SQL

---
+++ server/db-migrations/001-init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    _id VARCHAR(36) PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    second_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(10),
+    phone_number VARCHAR(20),
+    birthday DATE,
+    passwordHash VARCHAR(100) NOT NULL,
+    user_id VARCHAR(36) UNIQUE NOT NULL,
+    createdAt INT NOT NULL
+);
